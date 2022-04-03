b5cb5243c6c4751ab0dc2029245d7eaf6d70c137
Divyansh Srivastav <d.srivastav@netskrt.io>
2022-04-03 22:13:55 +0530

    Modified files.

--- local-env-setup.sh
+++ local-env-setup.sh
@@ -13,37 +13,57 @@ echo "
 }
 
 build () {
+    if [ ! -f .env ];then 
+    echo "INFO: .env not found, creating it in the current context." 
+    echo "Please enter the GitHub PAT."
+    read PAT
+    echo "AMP_DOC_TOKEN=$PAT" > .env
+    else
+
+    PAT=$(grep "AMP_DOC_TOKEN" .env | awk -F"=" '{print $2}')
+    if [[ ! -n $PAT ]];then
+    echo "INFO: .env exists, but PAT value missing. Please, enter the GitHub PAT."
+    read PAT
+    echo "AMP_DOC_TOKEN=$PAT" > .env
+    fi
+    fi 
+
+    echo "Check and create required files/dirs, if doesn't exists."
+    [ ! -d dist/static/samples ] && mkdir -p dist/static/samples
+    [ ! -d dist/static/files ] && mkdir -p dist/static/files
+    [ ! -f dist/static/samples/samples.json ] && touch dist/static/samples/samples.json 
 
     echo "1/3 - Run: NPM INSTALL"
     docker-compose run amp.dev npm install || { echo "NPM INSTALL failed.";exit 1; }
     echo "2/3 - Run: NPM RUN BOOTSTRAP"
     docker-compose run amp.dev npm run bootstrap ||  { echo "NPM RUN BOOTSTRAP failed.";exit 1; }
     echo "3/3 - Run: NPM RUN DEVELOP"
-    docker-compose run amp.dev npm run develop ||  { echo "NPM RUN DEVELOP failed.";exit 1; }
+    docker-compose run -p 8080:8080 -p 8083:8083 amp.dev npm run develop ||  { echo "NPM RUN DEVELOP failed.";exit 1; }
 }
 
 reload () {
-    echo " Run Reload CMD: NPM RUN DEVELOP"
-    docker-compose run amp.dev npm run develop || { echo "RELOAD CMD: NPM RUN DEVELOP failed.";exit 1; }
+    echo "Run Reload CMD: NPM RUN DEVELOP"
+    docker-compose run -p 8080:8080 -p 8083:8083 amp.dev npm run develop || { echo "RELOAD CMD: NPM RUN DEVELOP failed.";exit 1; } 
 }
 clean () {
-    echo "This feature is in progress."
+    docker system prune 
 }
 
 TOTAL_ARGS=$#
 [[ $TOTAL_ARGS -ne 1 ]] && { echo "Incorrect usage. See help (--usage)"; exit 1; }
 
-INPUT=$1
 
+INPUT=$1
+LOG_PATH="/tmp/amp-dev-logs.txt"
 case $INPUT in 
     --help) 
         usage
         ;;
     --build)
-        build
+        build | tee -a $LOG_PATH 2>&1
         ;;
     --reload)
-        reload 
+        reload | tee -a $LOG_PATH 2>&1
         ;;
     --clean)
         clean
